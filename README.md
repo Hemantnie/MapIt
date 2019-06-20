03034a613de511cf0a6c366ac6d5c9ea0ac2a569
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-20 16:11:01 +0530

    Getting copied data from clipboard and using in our code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MapIt
+Open any address passed as a command line into google maps
+#Modules Used
+1>Using webbrowser module to open browser<br>
+2>sys to read arguments from command line
+3>pyperclip to copy from clipboard
--- mapit.py
+++ mapit.py
@@ -1,8 +1,15 @@
-import webbrowser,sys
+import webbrowser,sys,pyperclip
+
+address = ""
 
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
-    address.replace(',','');
-    address = "www.google.com/maps/place/{}/".format(address.replace(' ','+'))
-    print(address)
-    webbrowser.open(address)
+else:
+    address = pyperclip.paste()
+
+
+address.replace(',','');
+address = "www.google.com/maps/place/{}/".format(address.replace(' ','+'))
+print(address)
+webbrowser.open(address)
+
